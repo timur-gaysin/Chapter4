3bd5d7d3c0710a1afb9ff6b8954e3feb7fe3940e
Timur Gaysin <93344539+timur-gaysin@users.noreply.github.com>
2023-03-05 21:22:35 +0300

    Initial Commit

---
+++ Chapter4/Chapter4App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter4App.swift
+//  Chapter4
+//
+//  Created by Timur on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Chapter4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
