a0bb0588e455324a840912eb4cd3a1263026c79e
Timur Gaysin <93344539+timur-gaysin@users.noreply.github.com>
2023-03-10 09:21:41 +0300

    Add Playground and GCD in ConentView

--- Chapter4/ContentViewGCP.swift
+++ Chapter4/ContentViewGCP.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentViewGCP: View {
     @State var message = ""
     @State var results = ""
     @State var sliderValue = 0.0
@@ -26,6 +26,7 @@ struct ContentView: View {
                     let endTime = NSDate()
                     message = "Completed in \(endTime.timeIntervalSince(startTime as Date)) seconds"
                 }
+                
             }
             TextEditor(text: $results)
             Slider(value: $sliderValue)
@@ -34,17 +35,17 @@ struct ContentView: View {
     }
 }
 
-func fetchSomethingFromServer() -> String {
+private func fetchSomethingFromServer() -> String {
     Thread.sleep(forTimeInterval: 1)
     return "Hi there"
 }
 
-func processData(_ data: String) -> String {
+private func processData(_ data: String) -> String {
     Thread.sleep(forTimeInterval: 2)
     return data.uppercased()
 }
 
-func calculateFirstResult(_ data: String) -> String {
+private func calculateFirstResult(_ data: String) -> String {
     Thread.sleep(forTimeInterval: 3)
     let message = "Number of chars: \(String(data).count)"
     return message
@@ -57,6 +58,6 @@ func calculateSecondResult(_ data: String) -> String {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentViewGCP()
     }
 }
